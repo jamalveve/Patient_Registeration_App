b95c332a64dd3ab893b45971cf095ba158de2b93
Jamal <jamalveve01@gmail.com>
2025-05-14 17:24:09 +0530

    feat:added user list component to display registered users
    git commit -m "feat: add user list component and update App routing"
    
    git commit -m "feat: add user list component and update App routing"
    
    -- INSERT -- git

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import RegistrationForm from './Components/RegisterationForm';
+import UserList from './Components/UserList';
 export default function App() {
   return (
     <div>
-     
+     <UserList/>
       <Routes>
       <Route path="/register" element={<RegistrationForm />} />
       <Route path="*" element={<Navigate to="/register" replace />} />
