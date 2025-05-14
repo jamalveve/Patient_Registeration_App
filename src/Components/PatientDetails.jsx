902c415c660b6f44760820a3676cbe7e1399b734
Jamal <jamalveve01@gmail.com>
2025-05-14 22:32:14 +0530

    feat:Implemented Query records using raw SQL and updated protectedRouter

---
+++ src/Components/PatientDetails.jsx
@@ -0,0 +1,13 @@
+import UserList from "./PatientList";
+import SQLQueryPanel from "./SQLQueryPannel";
+
+export default function PatientDetails() {
+  return (
+    <div>
+      <UserList />
+      <SQLQueryPanel />
+    </div>
+  );
+}
+
+
